a8e42b122916696dbe63ddae3190502b296b47ec
Ravi Shankar <wafflespeanut@gmail.com>
2016-08-30 23:01:44 +0530

    Support for multiple methods, and leave the config file to hooker

--- label_response/__init__.py
+++ label_response/__init__.py
@@ -1,15 +1,12 @@
 import json
 
-def check_labels(api):
-    with open('config.json', 'r') as fd:
-        config = json.load(fd)
-
-    if not config['active']:
+def check_labels(api, config):
+    if not config.get('active'):
         return
 
-    labels = config['labels']
+    labels = config.get('labels', [])
     for label, comment in labels.items():
         if api.payload['label']['name'].lower() == label:
             api.post_comment(comment)
 
-method = check_labels
+methods = [check_labels]
