f0409731ee27cb9cd3265a513735cb436e6ff409
Ravi Shankar <wafflespeanut@gmail.com>
2016-08-31 13:03:36 +0530

    Initial check for existing labels, lowercase fix, and minor bug fix

--- label_response/__init__.py
+++ label_response/__init__.py
@@ -1,5 +1,8 @@
 
 def check_labels(api, config):
+    if api.payload['action'] != 'labeled':
+        return
+
     # FIXME: should this be configured for individual repos? (like 'label_notify')
     labels = config.get('labels', [])
     for label, comment in labels.items():
