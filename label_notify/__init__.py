4454cae1482235ea1fca36a4f702b0c43eaff68d
Ravi Shankar <wafflespeanut@gmail.com>
2016-09-02 15:20:11 +0530

    Update the handlers to use the API's helpers

--- label_notify/__init__.py
+++ label_notify/__init__.py
@@ -1,50 +1,30 @@
 
 def notify_watchers(api, config):
-    repos = config['repos']
+    repos = config.get('repos')
     if not (repos and api.payload.get('action') == 'labeled'):
         return
 
-    sender = api.payload['sender']['login'].lower()
-    node = api.get_matching_path(['user', 'login'])
-    creator = node['user']['login'].lower()
-    new_label = api.payload['label']['name'].lower()
     watchers_to_be_notified = []
+    sender, creator = api.get_sender_and_creator()
 
     existing_labels = filter(lambda label: label != new_label, api.get_labels())
-    node = api.get_matching_path(['owner', 'login'])
-    owner = node['owner']['login'].lower()
-    repo = node['name'].lower()
+    new_label = api.payload['label']['name'].lower()
 
-    for name in repos:
-        split = name.lower().split('/')
-        if len(split) == 2:
-            watcher_owner = split[0]
-            # wildcard match to notify label watchers for any label event
-            # triggered in a repo owned by a particular owner
-            watcher_repo = repo if split[1] == '*' else split[1]
-        elif split[0] == '*':
-            # wildcard match to notify these users for any labels listed,
-            # regardless of the owner or the repo
-            watcher_owner = owner
-            watcher_repo = repo
-        else:
-            continue        # invalid format
+    for name in api.get_matching_repos(repos):
+        for user, labels in repos[name].items():
+            user = user.lower()
+            labels = map(lambda name: name.lower(), labels)
+            # don't notify if the user's an author, or if the user is
+            # the one who has triggered the label event
+            if user == sender or user == creator:
+                continue
 
-        if watcher_owner == owner and watcher_repo == repo:
-            for user, labels in repos[name].items():
-                user = user.lower()
-                labels = map(lambda name: name.lower(), labels)
-                # don't notify if the user's an author, or if the user is
-                # the one who has triggered the label event
-                if user == sender or user == creator:
-                    continue
+            if any(label in existing_labels for label in labels):
+                continue    # we've already notified the user
 
-                if any(label in existing_labels for label in labels):
-                    continue    # we've already notified the user
-
-                # If we don't find any labels, then notify the user for any labelling event
-                if not labels or new_label in labels:
-                    watchers_to_be_notified.append(user)
+            # If we don't find any labels, then notify the user for any labelling event
+            if not labels or new_label in labels:
+                watchers_to_be_notified.append(user)
 
     if watchers_to_be_notified:
         mentions = map(lambda name: '@%s' % name, watchers_to_be_notified)
--- label_response/__init__.py
+++ label_response/__init__.py
@@ -1,12 +1,14 @@
 
 def check_labels(api, config):
-    if api.payload.get('action') != 'labeled':
+    repos = config.get('repos')
+    if not (repos and api.payload.get('action') == 'labeled'):
         return
 
-    # FIXME: should this be configured for individual repos? (like 'label_notify')
-    labels = config.get('labels', [])
-    for label, comment in labels.items():
-        if api.payload['label']['name'].lower() == label:
-            api.post_comment(comment)
+    for name in api.get_matching_repos(repos):
+        labels = repos[name]
+        for label, comment in labels.items():
+            if api.payload['label']['name'].lower() == label:
+                api.post_comment(comment)
+
 
 methods = [check_labels]
