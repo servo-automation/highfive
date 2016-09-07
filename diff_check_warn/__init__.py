4e940f4639bbb3712e5ca80efb636df47d89f8bd
Ravi Shankar <wafflespeanut@gmail.com>
2016-09-07 16:53:50 +0530

    New handler for checking added lines in diff

---
+++ diff_check_warn/__init__.py
@@ -0,0 +1,21 @@
+
+def check_lines(api, config):
+    repos = config.get('repos')
+    pr = api.payload.get('pull_request')
+    if not (repos and pr and api.payload.get('action') == 'opened'):
+        return
+
+    for config in api.get_matches_from_config(repos):
+        matches = config.items()
+        lines = api.get_added_lines()
+
+        # FIXME: Change this to regex?
+        messages = [msg for line in lines for match, msg in matches if match in line]
+
+        if messages:
+            lines = '\n'.join(map(lambda line: ' * %s' % line, messages))
+            comment = ':warning: **Warning!** :warning:\n\n%s' % lines
+            api.post_comment(comment)
+
+
+methods = [check_lines]
