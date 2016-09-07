b09e5a84f488b32b10c17a8e6ff674de10db2f67
Ravi Shankar <wafflespeanut@gmail.com>
2016-09-07 21:35:03 +0530

    Support checking files in the diff

--- diff_check_warn/__init__.py
+++ diff_check_warn/__init__.py
@@ -1,17 +1,28 @@
 import re
 
 
-def check_lines(api, config):
+def check_diff(api, config):
     repos = config.get('repos')
     pr = api.payload.get('pull_request')
     if not (repos and pr and api.payload.get('action') == 'opened'):
         return
 
+    messages = set()    # so that we filter duplicates
     repo_config = api.get_matches_from_config(repos)
-    matches = repo_config.items()
-    lines = api.get_added_lines()
 
-    messages = [msg for line in lines for match, msg in matches if re.search(match, line)]
+    def get_messages(lines, matches):
+        for line in lines:
+            for match, msg in matches.items():
+                if re.search(match, line):
+                    messages.update([msg])
+
+    matches = repo_config['content']
+    lines = api.get_added_lines()
+    get_messages(lines, matches)
+
+    matches = repo_config['files']
+    paths = api.get_changed_files()
+    get_messages(paths, matches)
 
     if messages:
         lines = '\n'.join(map(lambda line: ' * %s' % line, messages))
@@ -19,4 +30,4 @@ def check_lines(api, config):
         api.post_comment(comment)
 
 
-methods = [check_lines]
+methods = [check_diff]
