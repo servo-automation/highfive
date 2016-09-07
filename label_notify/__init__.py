894273527e2d35fa27971d026d01869d9d3ca588
Ravi Shankar <wafflespeanut@gmail.com>
2016-09-07 17:22:37 +0530

    Fix per-repo config matching

--- label_notify/__init__.py
+++ label_notify/__init__.py
@@ -9,22 +9,22 @@ def notify_watchers(api, config):
 
     existing_labels = filter(lambda label: label != new_label, api.get_labels())
     new_label = api.payload['label']['name'].lower()
+    repo_config = api.get_matches_from_config(repos)
 
-    for config in api.get_matches_from_config(repos):
-        for user, labels in config.items():
-            user = user.lower()
-            labels = map(lambda name: name.lower(), labels)
-            # don't notify if the user's an author, or if the user is
-            # the one who has triggered the label event
-            if user == sender or user == creator:
-                continue
+    for user, labels in repo_config.items():
+        user = user.lower()
+        labels = map(lambda name: name.lower(), labels)
+        # don't notify if the user's an author, or if the user is
+        # the one who has triggered the label event
+        if user == sender or user == creator:
+            continue
 
-            if any(label in existing_labels for label in labels):
-                continue    # we've already notified the user
+        if any(label in existing_labels for label in labels):
+            continue    # we've already notified the user
 
-            # If we don't find any labels, then notify the user for any labelling event
-            if not labels or new_label in labels:
-                watchers_to_be_notified.append(user)
+        # If we don't find any labels, then notify the user for any labelling event
+        if not labels or new_label in labels:
+            watchers_to_be_notified.append(user)
 
     if watchers_to_be_notified:
         mentions = map(lambda name: '@%s' % name, watchers_to_be_notified)
--- label_response/__init__.py
+++ label_response/__init__.py
@@ -4,10 +4,11 @@ def check_labels(api, config):
     if not (repos and api.payload.get('action') == 'labeled'):
         return
 
-    for labels in api.get_matches_from_config(repos):
-        for label, comment in labels.items():
-            if api.payload['label']['name'].lower() == label:
-                api.post_comment(comment)
+    repo_config = api.get_matches_from_config(repos)
+
+    for label, comment in repo_config.items():
+        if api.payload['label']['name'].lower() == label:
+            api.post_comment(comment)
 
 
 methods = [check_labels]
