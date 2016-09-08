4260047f2882582e7a0ffdccc1c91f11212aa035
Ravi Shankar <wafflespeanut@gmail.com>
2016-09-08 18:40:32 +0530

    Add a Servo-specific handler (for checking new WPT metadata files)

--- diff_check_warn/__init__.py
+++ diff_check_warn/__init__.py
@@ -25,6 +25,41 @@ def _check_tests(repo_config, paths):
         return message % '%s and %s' % (', '.join(no_tests), last)
 
 
+# Servo-specific WPT metadata checker
+def _check_metadata(paths):
+    msg = ('This pull request adds file(s) to `%s` without the `.ini` extension. '
+           'Please consider removing the file(s)!')
+
+    metadata_dirs = ['tests/wpt/metadata', 'tests/wpt/mozilla/meta']
+    ignored = ['.ini', 'MANIFEST.json', 'mozilla-sync']
+    offending_file_dirs = set()
+
+    for path in paths:
+        if '.' in path and not any(re.search(f, path) for f in ignored):
+            offending_file_dirs |= set(d for d in metadata_dirs if re.search(d, path))
+
+    if offending_file_dirs:
+        if len(offending_file_dirs) == 1:
+            test_dirs = offending_file_dirs.pop()
+        else:
+            test_dirs = '{} and {}'.format(*offending_file_dirs)
+        return msg % test_dirs
+
+
+# define and append the repo-specific handlers here
+REPO_SPECIFIC_HANDLERS = {
+    "servo/servo": {
+        "lines": [
+            # content-checking methods
+        ],
+        "paths": [
+            # path-checking methods
+            _check_metadata
+        ]
+    },
+}
+
+
 def check_diff(api, config):
     repos = config.get('repos')
     pr = api.payload.get('pull_request')
@@ -51,6 +86,15 @@ def check_diff(api, config):
     test_check_result = _check_tests(repo_config, paths)
     if test_check_result:
         messages.update([test_check_result])
+
+    # run the repo-specific handlers (if any)
+    handlers = api.get_matches_from_config(REPO_SPECIFIC_HANDLERS)
+    for name, methods in handlers.items():
+        for method in methods:
+            _input = locals().get(name)
+            result = method(_input)
+            if result:
+                messages.update([result])
 
     if messages:
         lines = '\n'.join(map(lambda line: ' * %s' % line, messages))
