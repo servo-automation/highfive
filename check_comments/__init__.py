84589c382c9d3eb1bc6477eeff1ece28df59c08b
Ravi Shankar <wafflespeanut@gmail.com>
2016-09-09 13:06:38 +0530

    Import and refactor failure log posting from old highfive

--- check_comments/__init__.py
+++ check_comments/__init__.py
@@ -1,4 +1,45 @@
-import re
+from HTMLParser import HTMLParser
+
+import json, re
+
+
+def check_failure_log(api, comment):
+    # bors's comment would be something like,
+    # ":broken_heart: Test failed - [linux2](http://build.servo.org/builders/linux2/builds/2627)"
+    # ... from which we get the relevant build result url
+    url = iter(re.findall(r'.*\((.*)\)', str(comment))).next()
+    if not url:
+        return
+
+    # Substitute and get the new url
+    # (e.g. http://build.servo.org/json/builders/linux2/builds/2627)
+    json_url = re.sub(r'(.*)(builders/.*)', r'\1json/\2', url)
+    json_stuff = api.get_page_content(json_url)
+    if not json_stuff:
+        return
+
+    build_stats = json.loads(json_stuff)
+    failure_regex = r'.*Tests with unexpected results:\n(.*)\n</span><span'
+    comments = []
+
+    for step in build_stats['steps']:
+        if 'failed' not in step['text']:
+            continue
+
+        for name, log_url in step['logs']:
+            if name != 'stdio':
+                continue
+
+            stdio = api.get_page_content(log_url)
+            failures = iter(re.findall(failure_regex, stdio, re.DOTALL)).next()
+            failures = HTMLParser().unescape(failures)
+
+            if failures:
+                comment = [' ' * 4 + line for line in failures.split('\n')]
+                comments.extend(comment)
+
+    if comments:
+        api.post_comment('\n'.join(comments))
 
 
 def _find_reviewer(api):
@@ -6,7 +47,7 @@ def _find_reviewer(api):
     comment = api.payload['comment']['body']
 
     def get_approver():
-        approval_regex = r'.*@bors-servo[: ]*r([\+=])([a-zA-Z0-9\-,]*)'
+        approval_regex = r'.*@bors-servo[: ]*r([\+=])([a-zA-Z0-9\-,\+]*)'
         approval = re.search(approval_regex, str(comment))
 
         if approval:
@@ -25,11 +66,36 @@ def _find_reviewer(api):
         api.set_assignees(reviewers)
 
 
+def _watch_bors(api):
+    user = api.payload['comment']['user']['login']
+    comment = api.payload['comment']['body']
+
+    if user != 'bors-servo':
+        return
+
+    labels = api.get_labels()
+    if 'has been approved by' in comment or 'Testing commit' in comment:
+        remove_labels = ['S-awaiting-review', 'S-needs-rebase',
+                         'S-tests-failed', 'S-needs-code-changes',
+                         'S-needs-squash', 'S-awaiting-answer']
+        api.update_labels(added=['S-awaiting-merge'], removed=remove_labels)
+
+    elif 'Test failed' in comment:
+        api.update_labels(added=['S-tests-failed'], removed=['S-awaiting-merge'])
+        # Get the homu build stats url,
+        # extract the failed tests and post them!
+        check_failure_log(api, comment)
+
+    elif 'Please resolve the merge conflicts' in comment:
+        api.update_labels(added=['S-needs-rebase'], removed=['S-awaiting-merge'])
+
+
 # All these handlers are specific to Servo!
 REPO_SPECIFIC_HANDLERS = {
     "servo/servo": {
         "methods": [
-            _find_reviewer
+            _find_reviewer,
+            _watch_bors,
         ]
     },
 }
