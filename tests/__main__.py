d6ac6a45cc254b129ec8aded2fad848e9adf6c9f
Ravi Shankar <wafflespeanut@gmail.com>
2018-03-18 17:04:48 +0530

    Properly exit when an unittest fails

--- tests/__main__.py
+++ tests/__main__.py
@@ -1,5 +1,8 @@
+from unittest import TestSuite, TextTestResult, TextTestRunner
+
 import imp
 import logging
+import sys
 import unittest
 
 if __name__ == '__main__':
@@ -7,15 +10,28 @@ if __name__ == '__main__':
                                ('', '', imp.PKG_DIRECTORY))
     logging.basicConfig(level=logging.CRITICAL)
 
-    from config_tests import ConfigurationTests
-    from installation_manager_tests import InstallationManagerTests
-    from runner_tests import RunnerTests
+    test_suite = TestSuite()
+
     from api_provider_tests import APIProviderTests
+    from config_tests import ConfigurationTests
     from event_handler_tests import EventHandlerTests
+    from installation_manager_tests import InstallationManagerTests
     from json_store_tests import JsonStoreTests
+    from runner_tests import RunnerTests
+
+    test_suite.addTests(unittest.makeSuite(APIProviderTests))
+    test_suite.addTests(unittest.makeSuite(ConfigurationTests))
+    test_suite.addTests(unittest.makeSuite(EventHandlerTests))
+    test_suite.addTests(unittest.makeSuite(InstallationManagerTests))
+    test_suite.addTests(unittest.makeSuite(JsonStoreTests))
+    test_suite.addTests(unittest.makeSuite(RunnerTests))
+
+    test_runner = TextTestRunner(resultclass=TextTestResult, verbosity=2)
+    unittest_result = test_runner.run(test_suite)
 
     import handler_tests
-
-    unittest.main(exit=False)       # TODO: This doesn't exit when failure occurs. Use the builder.
     print
     handler_tests.run()
+
+    if not unittest_result.wasSuccessful():
+        sys.exit(1)
